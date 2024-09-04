516209c0b6e21f008f7b449a7b3501cea486c17f
Kevin <hello@kevinivan.com>
2024-09-04 17:51:31 +0200

    add hotjar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import localFont from "next/font/local"
 import { Toaster } from "@/components/ui/sonner"
 import "./globals.css"
+import Hotjar from "@/components/Hotjar"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,6 +31,7 @@ export default function RootLayout({
       >
         {children}
         <Toaster />
+        <Hotjar />
       </body>
     </html>
   )
