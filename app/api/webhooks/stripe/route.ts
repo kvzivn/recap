743f8127858cc321b6657e69b5d363d4016d675e
Kevin <hello@kevinivan.com>
2024-09-10 16:13:54 +0200

    revert stripe webhook

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -3,54 +3,25 @@ import { NextResponse } from "next/server"
 import stripe from "stripe"
 
 export async function POST(request: Request) {
-  console.log("Webhook received")
-  console.log("NODE_ENV:", process.env.NODE_ENV)
-
   const body = await request.text()
-  console.log("Raw body:", body.substring(0, 100) + "...") // Log first 100 chars
 
   const sig = request.headers.get("stripe-signature") as string
-  console.log("Stripe signature:", sig)
-
-  let endpointSecret: string | undefined
-  if (process.env.NODE_ENV === "production") {
-    endpointSecret = process.env.STRIPE_LIVE_WEBHOOK_SECRET
-    console.log("Using LIVE webhook secret")
-  } else {
-    endpointSecret = process.env.STRIPE_TEST_WEBHOOK_SECRET
-    console.log("Using TEST webhook secret")
-  }
-
-  if (!endpointSecret) {
-    console.error("Webhook secret is undefined")
-    return NextResponse.json(
-      { message: "Server configuration error" },
-      { status: 500 }
-    )
-  }
-
-  console.log(
-    "Endpoint secret defined:",
-    endpointSecret.substring(0, 5) + "..."
-  )
+  const endpointSecret =
+    process.env.NODE_ENV === "production"
+      ? process.env.STRIPE_WEBHOOK_SECRET_LIVE!
+      : process.env.STRIPE_WEBHOOK_SECRET_TEST!
 
   let event
 
   try {
     event = stripe.webhooks.constructEvent(body, sig, endpointSecret)
-    console.log("Event constructed successfully:", event.type)
   } catch (err) {
-    console.error("Webhook error:", err)
-    return NextResponse.json(
-      { message: "Webhook error", error: err },
-      { status: 400 }
-    )
+    return NextResponse.json({ message: "Webhook error", error: err })
   }
 
   const eventType = event.type
 
   if (eventType === "checkout.session.completed") {
-    console.log("Checkout session completed")
     const { id, metadata } = event.data.object
 
     const transaction = {
