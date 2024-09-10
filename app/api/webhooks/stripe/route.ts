67ea6ba9ddfbe6957c81091ce2d0b4af27dc2bcc
Kevin <hello@kevinivan.com>
2024-09-10 20:06:12 +0200

    were live again

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -5,7 +5,7 @@ import stripe from "stripe"
 export async function POST(request: Request) {
   const body = await request.text()
   const sig = request.headers.get("stripe-signature") as string
-  const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET_TEST!
+  const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET_LIVE!
 
   let event
 
