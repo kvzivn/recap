287c9972277dbecd110ca072cfbd16a4408d6f9d
Kevin <hello@kevinivan.com>
2024-09-13 15:01:11 +0200

    test cron job

--- app/api/cron/schedule-emails/route.ts
+++ app/api/cron/schedule-emails/route.ts
@@ -1,9 +1,10 @@
-import { scheduleEmail } from "@/lib/actions/email.actions"
+import { sendEmail } from "@/lib/actions/email.actions"
 import { getLoggedInUser } from "@/lib/actions/user.actions"
 
 export const dynamic = "force-dynamic"
 
 export async function GET() {
   const user = await getLoggedInUser()
-  await scheduleEmail(user.$id)
+  await sendEmail(user.$id)
+  // await scheduleEmail(user.$id)
 }
